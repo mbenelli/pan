4ba4e6ec34eb15a41d82550f7fea36d890a0d437
Marco Benelli <marco.benelli@mercedes-benz.com>
2024-11-01 07:54:40 +0100

    jenkins: update test

--- jenkins/jenkins_test.go
+++ jenkins/jenkins_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestJenkinsConf(t *testing.T) {
 	sample := `{"Url": "foo", "Jobs": ["bar0", "bar1", "bar2"]}`
-	var j Jenkins
+	var j Config
 	err := json.Unmarshal([]byte(sample), &j)
 	if err != nil {
 		t.Fatalf("Json error: %v", err)
